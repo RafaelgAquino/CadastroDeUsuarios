fe55ddf6007827247ce353418a09d8c8e7a80bee
Rafael Aquino <rafael.aquino@seduh.local>
2025-10-10 15:13:08 -0300

    first commit

---
+++ src/main/java/br/gov/br/seduh/Cadastro/CadastroApplication.java
@@ -0,0 +1,13 @@
+package br.gov.br.seduh.Cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/br/seduh/Cadastro/CadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.br.seduh.Cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
