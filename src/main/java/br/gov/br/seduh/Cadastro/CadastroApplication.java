a7952b3a3d6e2dd2af92e484d874c5ed09db41e2
Rafael Aquino <rafael.aquino@seduh.local>
2025-10-13 10:59:22 -0300

    Controller Criada

--- src/main/java/br/gov/br/seduh/Cadastro/CadastroApplication.java
+++ src/main/java/br/gov/br/seduh/Cadastro/CadastroApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CadastroApplication {
+public class  CadastroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroApplication.class, args);
