71e53f8e4be1935f3e3235b65840a29d65edcf2a
Rafael Aquino <rafael.aquino@seduh.local>
2025-10-15 10:01:04 -0300

    feat: Coloca o lombok nas classes para abstração

--- src/main/java/br/gov/br/seduh/Cadastro/Tasks/TasksModel.java
+++ src/main/java/br/gov/br/seduh/Cadastro/Tasks/TasksModel.java
@@ -2,9 +2,15 @@ package br.gov.br.seduh.Cadastro.Tasks;
 
 import br.gov.br.seduh.Cadastro.Users.UserModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Table (name ="tb_tasks")
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TasksModel {
 
     @Id
--- src/main/java/br/gov/br/seduh/Cadastro/Users/UserModel.java
+++ src/main/java/br/gov/br/seduh/Cadastro/Users/UserModel.java
@@ -2,9 +2,15 @@ package br.gov.br.seduh.Cadastro.Users;
 
 import br.gov.br.seduh.Cadastro.Tasks.TasksModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class UserModel {
 
     @Id
@@ -17,39 +23,4 @@ public class UserModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id ")
     private TasksModel tasks;
-
-    public UserModel() {
-    }
-
-    public UserModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
