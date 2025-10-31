1e8f4c9b7211a032bf9e4cefaf401bc568ae1c57
Rafael Aquino <rafael.aquino@seduh.local>
2025-10-31 10:37:32 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/br/gov/br/seduh/Cadastro/Users/UserController.java
+++ src/main/java/br/gov/br/seduh/Cadastro/Users/UserController.java
@@ -1,8 +1,6 @@
 package br.gov.br.seduh.Cadastro.Users;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,5 +10,33 @@ public class UserController {
     public String boasVindas() {
         return "Primeira mensagem na rota";
     }
-    
+
+    //Adicionar usuário (CREATE)
+    @PostMapping ("/criar")
+    public String criarUsuario() {
+        return "Usuário criado";
+    }
+
+    //Mostrar todos os usuários (READ)
+    @GetMapping ("/todos")
+    public String mostrarTodosOsUsuarios() {
+        return "Mostrar usuário";
+    }
+
+    //Mostrar usuários por id (READ)
+    @GetMapping ("/todosID")
+    public String mostrarTodosOsUsuariosPorId() {
+        return "Mostrar usuário por id";
+    }
+
+    //Alterar dados dos usuários (UPDATE)
+    @PutMapping ("/alterarID")
+    public String alterarUsuarioPorId() {
+         return "Alterar usuário por id";
+    }
+    //Deletar usuário (DELETE)
+    @DeleteMapping ("/deletarID")
+    public String deletarUsuarioPorId() {
+        return "Deletar usuário por id";
+    }
 }
