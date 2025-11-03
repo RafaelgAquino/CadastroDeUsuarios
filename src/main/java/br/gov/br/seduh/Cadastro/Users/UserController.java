522d4e8c8176bc73007a1987388d73d84ce939e3
Rafael Aquino <rafael.aquino@seduh.local>
2025-11-03 10:56:28 -0300

    feat: Altera a rota do controller para listar os usuários e injeta a dependência do Service e do Repository

--- src/main/java/br/gov/br/seduh/Cadastro/Users/UserController.java
+++ src/main/java/br/gov/br/seduh/Cadastro/Users/UserController.java
@@ -2,9 +2,17 @@ package br.gov.br.seduh.Cadastro.Users;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/usuarios")
 public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping ("/boasvindas")
     public String boasVindas() {
@@ -18,9 +26,9 @@ public class UserController {
     }
 
     //Mostrar todos os usuários (READ)
-    @GetMapping ("/listarID")
-    public String mostrarTodosOsUsuarios() {
-        return "Mostrar usuário";
+    @GetMapping ("/listar")
+    public List listar() {
+        return userService.listarUsuarios();
     }
 
     //Mostrar usuários por id (READ)
--- src/main/java/br/gov/br/seduh/Cadastro/Users/UserService.java
+++ src/main/java/br/gov/br/seduh/Cadastro/Users/UserService.java
@@ -1,4 +1,21 @@
 package br.gov.br.seduh.Cadastro.Users;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    //Listar todos os usuários
+
+    public List<UserModel> listarUsuarios() {
+        return userRepository.findAll();
+    }
 }
