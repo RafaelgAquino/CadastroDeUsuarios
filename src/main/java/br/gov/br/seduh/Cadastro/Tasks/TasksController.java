1279b5d77ae748373dc2a2d2b19632611d3f4000
Rafael Aquino <rafael.aquino@seduh.local>
2025-11-13 10:42:38 -0300

    feat: Adiciona CRUD de tarefas

--- src/main/java/br/gov/br/seduh/Cadastro/Tasks/TasksController.java
+++ src/main/java/br/gov/br/seduh/Cadastro/Tasks/TasksController.java
@@ -1,32 +1,40 @@
 package br.gov.br.seduh.Cadastro.Tasks;
 
+import br.gov.br.seduh.Cadastro.Users.UserService;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
-@RequestMapping("tarefas")
+@RequestMapping("/tarefas")
 public class TasksController {
+
+    private TasksService tasksService;
+
+    public TasksController(TasksService tasksService) {
+        this.tasksService = tasksService;
+    }
 
     //GET -- Mandar uma requisição para mostrar as tarefas
     @GetMapping("/listar")
-    public String listarTarefas() {
-        return "Tarefas listadas com sucesso";
+    public List listarTarefas() {
+        return tasksService.listarTarefas();
     }
 
     //POST -- Mandar uma requisição para criar as tarefas
     @PostMapping("/criar")
-    public String criarTarefa() {
-        return "Tarefa criada com sucesso";
+    public TasksModel criarTarefa(@RequestBody TasksModel tarefa) {
+        return tasksService.criarTarefa(tarefa);
     }
 
     //PUT -- Mandar uma requisição para alterar/criar as tarefas
-    @PutMapping("/alterar")
-    public String alterarTarefa() {
-        return "Tarefa alterada com sucesso";
+    @PutMapping("/alterar/{id}")
+    public TasksModel alterarTarefa(@PathVariable Long id, @RequestBody TasksModel tarefaAtualizada) {
+        return tasksService.alterarTarefaPorId(id, tarefaAtualizada);
     }
 
     //DELETE -- Mandar uma requisição para deletar as tarefas
-    @DeleteMapping ("/deletar")
-    public String deletarTarefa() {
-        return "Tarefa deletada com sucesso";
+    @DeleteMapping ("/deletar/{id}")
+    public void deletarTarefaPorId(@PathVariable Long id) {
+        tasksService.deletarTarefaPorId(id);
     }
 }
--- src/main/java/br/gov/br/seduh/Cadastro/Tasks/TasksService.java
+++ src/main/java/br/gov/br/seduh/Cadastro/Tasks/TasksService.java
@@ -1,4 +1,45 @@
 package br.gov.br.seduh.Cadastro.Tasks;
 
+import br.gov.br.seduh.Cadastro.Tasks.TasksModel;
+import br.gov.br.seduh.Cadastro.Tasks.
+        TasksRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TasksService {
+    private TasksRepository tasksRepository;
+
+    public TasksService(TasksRepository tasksRepository) {
+        this.tasksRepository = tasksRepository;
+    }
+
+    //Listar todos os usuários
+
+    public List<TasksModel> listarTarefas() {
+        return tasksRepository.findAll();
+    }
+
+    public TasksModel  listarTarefasPorId(Long id) {
+        Optional<TasksModel> taskPorId = tasksRepository.findById(id);
+        return taskPorId.orElse(null);
+    }
+    //Criar usuário (CREATE)
+    public TasksModel criarTarefa(TasksModel tarefa) {
+        return tasksRepository.save(tarefa);
+    }
+    //Deletar usuário (DELETE)
+    public void deletarTarefaPorId (Long id) {
+        tasksRepository.deleteById(id);
+    }
+    //Alterar usuário (UPDATE)
+    public TasksModel alterarTarefaPorId (Long id, TasksModel tarefaAtualizada) {
+        if (tasksRepository.existsById(id)) {
+            tarefaAtualizada.setId(id);
+            return tasksRepository.save(tarefaAtualizada);
+        }
+        return null;
+    }
 }
